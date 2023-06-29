dfda6e2a322388afe3277810a82330ae6ac89691
Stephen <nwabuezesc@gmail.com>
2023-06-29 15:22:11 +0100

    creathing the destop view of the landing page

--- src/components/homeComponents/review/CustomReviews.jsx
+++ src/components/homeComponents/review/CustomReviews.jsx
@@ -1,19 +1,23 @@
 import React from "react";
+import Rating from "../../Rating";
+import customerImage from "../../../assets/Images/customersImage.png"
 
 export default function CustomReviews({ comments }) {
   return (
     <>
-      <section className=" md:flex justify-between">
+      <section className="grid grid-cols-4 gap-2">
         {comments.map(({ id, author, text }) => (
           <div
             key={id}
-            className=" border rounded-md mx-5 items-center min-w-44 lg:w-48 px-2 py-3 mt-5"
+            className="shadow-2xl rounded-md mx-5 items-center px-2 py-3 mt-5"
           >
-            <p className="text-yellow-400 ">
-              {author} {/* AUTHOR WOULD COME FROM THE FORM REVIEW NAME */}
-            </p>
+            <div className="flex space-x-2 items-center ">
+              <img src={customerImage} height={35} width={35} />  
+              <p className="text-yellow-400 font-medium ">{author} </p>
+            </div>
+            <Rating rate={4} />
 
-            <p className="text-sm text-white">{text}</p>
+            <p className="text-sm text-white text-justify">{text}</p>
           </div>
         ))}
       </section>
