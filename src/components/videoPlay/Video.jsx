d1a6ecc42688c2e9d215241b74e2f32fa6f1d0a2
Hassaan Baig <hassaanb000@gmail.com>
2023-09-22 19:16:00 +0500

    Final UI look like the Figma

--- src/components/videoPlay/Video.jsx
+++ src/components/videoPlay/Video.jsx
@@ -89,81 +89,8 @@ const Video = ({ fullScreen, handleFullScreen }) => {
     setHiddenVolumn(!hiddenVolumn);
   };
   return (
-    // <div className=" w-full h-full">
-    //   <div className="relative lg:h-[24.3rem] xl:h-[37rem] sm:h-[27rem]">
-    //     <video
-    //       src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
-    //       //    className={`${isPlaying ? 'w-full h-full' : "hidden"}`}
-    //       className="w-full h-full"
-    //       id="video"
-    //       ref={videoRef}
-    //       onTimeUpdate={handleTime}
-    //       onClick={handlePlayandPause}
-    //       poster={Background}
-    //       onMouseOver={() => setHiddenControl(!hiddenControl)}
-    //       onMouseOut={() => setHiddenControl(!hiddenControl)}
-    //     ></video>
-    //   </div>
-    //   <img
-    //     src={controlImage}
-    //     onClick={handlePlayandPause}
-    //     className={`w-20 h-[75.88px] rounded-full z-50 absolute top-28 left-1/3 sm:top-60 sm:left-[21rem] xl:top-[21rem] xl:left-[30rem] ${
-    //       isPlaying ? "hidden" : "block"
-    //     } `}
-    //   />
-    //   <div
-    //     className={`absolute sm:w-[92%] sm:bottom-[3.6rem] bottom-[16.8rem] lg:bottom-[5.6rem] xl:bottom-[6.9rem] 2xl:bottom-[5.9rem] xl:gap-6 lg:w-[65%] gap-3 md:mx-5 sm:mx-2 md:w-2/3 w-full items-center flex`}
-    //   >
-    //     <p className="text-[#ffff] text-smR w-16 xl:font-medium  max-sm:w-20">
-    //       <span className="">{currentMinutes}</span> /{" "}
-    //       <span className="">{duration}</span>
-    //     </p>
-    //     {/* <progress max="100" min="0" className="md:w-[65%] h-2 rounded-full bg-[#FFFFF] [&::-moz-progress-bar]:bg-[#E72020] cursor-pointer " onClick={handleProgressBarClick} value={progress}></progress> */}
-    //     <div className="w-[79%] rounded-full h-2 cursor-pointer bg-gray-300">
-    //       <div
-    //         className="h-2 bg-[#E72020] rounded-full"
-    //         style={{ width: progress }}
-    //         onClick={handleProgressBarClick}
-    //       ></div>
-    //     </div>
-    //     <div>
-    //       <img
-    //         src={volumnImage}
-    //         alt="volumn"
-    //         className="w-6 cursor-pointer"
-    //         onClick={HandleVolumnHidden}
-    //       />
-    //       <input
-    //         type="range"
-    //         min="0"
-    //         max="1"
-    //         value={volume}
-    //         onChange={handleVolume}
-    //         step="0.1"
-    //         className={`${
-    //           hiddenVolumn ? "block" : "hidden"
-    //         } transform -rotate-90 origin-left absolute bottom-[14px] ml-3`}
-    //       />
-    //     </div>
-    //     <div>
-    //       <img src={cc} alt="cc" className="w-6 cursor-pointer" />
-    //     </div>
-    //     <div>
-    //       <img src={setting} alt="setting" className="w-6 cursor-pointer" />
-    //     </div>
-    //     <div>
-    //       <img
-    //         src={reszing}
-    //         alt="reszing"
-    //         className="w-6 cursor-pointer"
-    //         onClick={() => handleFullScreen()}
-    //       />
-    //     </div>
-    //   </div>
-    // </div>
-    // <div className="w-[98%] max-w-[900px] bg-black overflow-hidden">
       <div 
-        className="relative h-fit"
+        className="relative h-fit flex justify-center items-center"
         onMouseOver={() => setHiddenControl(!hiddenControl)}
         onMouseOut={() => setHiddenControl(!hiddenControl)}
       >
@@ -178,7 +105,9 @@ const Video = ({ fullScreen, handleFullScreen }) => {
         poster={Background}
         />
       <div className="absolute left-0 right-0 bottom-5 z-50 mx-2">
-        <ul className="flex items-center justify-around mx-3">
+        <ul
+        className={`${hiddenControl ? "flex items-center justify-around mx-3" : "flex items-center justify-around mx-3" } `}
+        >
           <li>
           <p className="text-[#ffff] text-smR w-16 xl:font-medium  max-sm:w-20">
           <span className="">{currentMinutes}</span> /{" "}
@@ -186,13 +115,11 @@ const Video = ({ fullScreen, handleFullScreen }) => {
          </p>
           </li>
           <li className="w-[64%] rounded-full h-2 cursor-pointer bg-gray-300">
-          {/* <div className=""> */}
           <div
              className="h-2 bg-[#E72020] rounded-full"
              style={{ width: progress }}
              onClick={handleProgressBarClick}
            ></div>
-         {/* </div> */}
           </li>
           <li>
             <button type="button">
@@ -238,7 +165,6 @@ const Video = ({ fullScreen, handleFullScreen }) => {
         </ul>
       </div>
       </div>
-    // </div>
   );
 };
 
