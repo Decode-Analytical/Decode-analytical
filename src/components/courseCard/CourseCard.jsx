15746e63317478352984928176f158eb7e52cbbf
Allassane <dickoallassane1997@gmail.com>
2023-09-23 12:19:18 +0100

    Add header in dashboard page

--- src/components/courseCard/CourseCard.jsx
+++ src/components/courseCard/CourseCard.jsx
@@ -6,45 +6,45 @@ const CourseCard = () => {
   const progress = 38
   return (
     <>
-    <div class="max-w-sm w-full lg:max-w-full lg:flex course-card">
-      <div class="h-48 lg:h-auto lg:w-80 flex-none bg-cover rounded text-center overflow-hidden img-container" title="Woman holding a mug">
+    <div className="max-w-sm w-full lg:max-w-full lg:flex course-card">
+      <div className="h-48 lg:h-auto lg:w-80 flex-none bg-cover rounded text-center overflow-hidden img-container" title="Woman holding a mug">
       </div>
-      <div class="bg-white flex flex-col flex-1 justify-between leading-normal">
-        <div class="mb-1 mt-6 lg:mx-4 lg:mt-0 flex flex-col justify-between">
-          <h3 class="text-gray-900 font-bold text-xl mb-2 ">Can coffee make you a better developer?</h3>
+      <div className="bg-white flex flex-col flex-1 justify-between leading-normal">
+        <div className="mb-1 mt-6 lg:mx-4 lg:mt-0 flex flex-col justify-between">
+          <h3 className="text-gray-900 font-bold text-xl mb-2 ">Can coffee make you a better developer?</h3>
           <div className='flex flex-row items-center gap-2'>
-            <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-              <div class="bg-blue-600 h-2.5 rounded-full dark:bg-blue-500" style={{width: `${progress}%`}}></div>
+            <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+              <div className="bg-blue-600 h-2.5 rounded-full dark:bg-blue-500" style={{width: `${progress}%`}}></div>
             </div>
             <p className='bold'>{progress}%</p>
           </div>
 
           <div className='flex flex-col flex-1 '>
             
-            <div class="flex items-center mt-5 mb-4">
-              <img class="w-10 h-10 rounded-full mr-4" src="https://resize.prod.femina.ladmedia.fr/rblr/1200,806/img/var/2023-03/cool-girl-crop.jpg" alt="Avatar of Jonathan Reinink"/>
-              <div class="text-sm">
-                <p class="text-gray-500 leading-none text-center m-0">by Jonathan Reinink</p>
+            <div className="flex items-center mt-5 mb-4">
+              <img className="w-10 h-10 rounded-full mr-4" src="https://resize.prod.femina.ladmedia.fr/rblr/1200,806/img/var/2023-03/cool-girl-crop.jpg" alt="Avatar of Jonathan Reinink"/>
+              <div className="text-sm">
+                <p className="text-gray-500 leading-none text-center m-0">by Jonathan Reinink</p>
               </div>
             </div>
             
-            <div class="flex items-center">
-                <svg class="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+            <div className="flex items-center">
+                <svg className="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
                     <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
                 </svg>
-                <svg class="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                <svg className="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
                     <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
                 </svg>
-                <svg class="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                <svg className="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
                     <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
                 </svg>
-                <svg class="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                <svg className="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
                     <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
                 </svg>
-                <svg class="w-4 h-4 text-gray-300 mr-1 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                <svg className="w-4 h-4 text-gray-300 mr-1 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
                     <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
                 </svg>
-                <p class="m-0 text-sm font-medium text-gray-500 dark:text-gray-400 text-center">4.95 (24)</p>
+                <p className="m-0 text-sm font-medium text-gray-500 dark:text-gray-400 text-center">4.95 (24)</p>
             </div>
 
             <div className='flex flex-row mt-4 max-w-lg'>
--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import CourseCard from '../../components/courseCard/courseCard';
 import ProfileSideBar from './ProfileSideBar';
+import { NavLink } from 'react-router-dom';
 
 
 const Profile = () => {
@@ -11,16 +12,22 @@ const Profile = () => {
     <>
       <div className="flex flex-row">
         <ProfileSideBar />
-        <div className="flex flex-col ml-[300px] p-12 flex-1">{/* column2 */}
-            <header>
-              <h2>Welcom to your dashboard</h2>
-              <nav>
-                <ul>
-                  <li>
-                    <a href="/">Dashbord</a>
+        <div className="flex flex-col ml-[300px] flex-1">{/* column2 */}
+            <header className='flex flex-col border-b-2 px-6 pt-4'>
+              <h2 className='font-bold text-2xl'>Welcom to your dashboard</h2>
+              <nav className='mt-3'>
+                <ul className='flex gap-4'>
+                  <li key="nav-dashboard">
+                    <NavLink end to="/profile/dashboard" 
+                      className={({isActive})=> `flex-1 pb-2 block  text-gray-400 ${isActive && "border-b-2 border-b-gray-950 font-semibold text-gray-950"}`}
+                      >Dashbord
+                    </NavLink>
                   </li>
-                  <li>
-                    <a href="/points">Points</a>
+                  <li key="nav-point">
+                    <NavLink to="/profile/dashboard/point" 
+                      className={({isActive})=> `flex-1 pb-2 block text-gray-400  ${isActive && "border-b-2 border-b-gray-950 font-semibold text-gray-950"}`}
+                      >Points
+                    </NavLink>
                   </li>
                 </ul>
               </nav>
