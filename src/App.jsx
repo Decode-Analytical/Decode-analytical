12a1e8665c1fe6cb1f4992a8e98f41868be09fc8
olayinkagrace <olayinka4grace@gmail.com>
2023-09-24 12:43:27 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import NavBar from "./components/homeComponents/navbar/NavBar";
 import HomePage from "./pages/homepage/HomePage";
 import LoginSignUp from "./pages/login/LoginSignUp";
 import PasswordReset from "./components/loginComponents/PasswordReset";
-import PremiumCourses from './pages/courseDetailPage/premiumCourses'
 import FreeCourses from './pages/courseDetailPage/freeCourses'
 import CartPage from "./pages/cartAndPaymentPage/CartPage";
 import PaymentPage from "./pages/cartAndPaymentPage/PaymentPage";
@@ -24,7 +23,7 @@ function App() {
                 <Route path='/' exact element = {<HomePage/>} />
                 <Route path='/LoginSignUp' element = {<LoginSignUp/>} />
                 <Route path='/PasswordReset' element = {<PasswordReset />} />
-                <Route path='/PremiumCourses' element = {<PremiumCourses />} />
+                
                 <Route path='/FreeCourses' element = {<FreeCourses />} />
                 <Route path='/CartPage' element = {<CartPage />} />
                 <Route path='/PaymentPage' element = {<PaymentPage />} />
