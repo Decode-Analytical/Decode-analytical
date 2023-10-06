47afae42df8ccc06d6d92fa392c4367613cadb5b
Allassane <dickoallassane1997@gmail.com>
2023-10-06 18:03:03 +0100

    Add mainSideBar

--- src/components/courseCard/EnrolledCourseCard.jsx
+++ src/components/courseCard/EnrolledCourseCard.jsx
@@ -61,7 +61,7 @@ const EnrolledCourseCard = (props) => {
                         <div className='flex flex-row mt-4 max-w-lg'>
                         <div className='flex flex-1 flex-row gap-1'>
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="21" viewBox="0 0 20 21" fill="none">
-                            <path d="M10 7.16667V10.5L12.5 13M17.5 10.5C17.5 14.6421 14.1421 18 10 18C5.85786 18 2.5 14.6421 2.5 10.5C2.5 6.35786 5.85786 3 10 3C14.1421 3 17.5 6.35786 17.5 10.5Z" stroke="black" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M10 7.16667V10.5L12.5 13M17.5 10.5C17.5 14.6421 14.1421 18 10 18C5.85786 18 2.5 14.6421 2.5 10.5C2.5 6.35786 5.85786 3 10 3C14.1421 3 17.5 6.35786 17.5 10.5Z" stroke="black" strokeWidth="1.66667" strokeLinecap="round" stroke-linejoin="round"/>
                             </svg>
                             <p className='text-sm'>
                             00h 00m left
@@ -71,7 +71,7 @@ const EnrolledCourseCard = (props) => {
                             {levelNumber ==0 && (
                             <>
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="21" viewBox="0 0 20 21" fill="none">
-                                <path d="M7.5 16.3333V11.3333C7.5 10.4129 6.75381 9.66667 5.83333 9.66667H4.16667C3.24619 9.66667 2.5 10.4129 2.5 11.3333V16.3333C2.5 17.2538 3.24619 18 4.16667 18H5.83333C6.75381 18 7.5 17.2538 7.5 16.3333ZM7.5 16.3333V8C7.5 7.07953 8.24619 6.33333 9.16667 6.33333H10.8333C11.7538 6.33333 12.5 7.07953 12.5 8V16.3333M7.5 16.3333C7.5 17.2538 8.24619 18 9.16667 18H10.8333C11.7538 18 12.5 17.2538 12.5 16.3333M12.5 16.3333V4.66667C12.5 3.74619 13.2462 3 14.1667 3H15.8333C16.7538 3 17.5 3.74619 17.5 4.66667V16.3333C17.5 17.2538 16.7538 18 15.8333 18H14.1667C13.2462 18 12.5 17.2538 12.5 16.3333Z" stroke="black" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M7.5 16.3333V11.3333C7.5 10.4129 6.75381 9.66667 5.83333 9.66667H4.16667C3.24619 9.66667 2.5 10.4129 2.5 11.3333V16.3333C2.5 17.2538 3.24619 18 4.16667 18H5.83333C6.75381 18 7.5 17.2538 7.5 16.3333ZM7.5 16.3333V8C7.5 7.07953 8.24619 6.33333 9.16667 6.33333H10.8333C11.7538 6.33333 12.5 7.07953 12.5 8V16.3333M7.5 16.3333C7.5 17.2538 8.24619 18 9.16667 18H10.8333C11.7538 18 12.5 17.2538 12.5 16.3333M12.5 16.3333V4.66667C12.5 3.74619 13.2462 3 14.1667 3H15.8333C16.7538 3 17.5 3.74619 17.5 4.66667V16.3333C17.5 17.2538 16.7538 18 15.8333 18H14.1667C13.2462 18 12.5 17.2538 12.5 16.3333Z" stroke="black" strokeWidth="1.66667" strokeLinecap="round" stroke-linejoin="round"/>
                             </svg>
                             <p className='text-sm'>Beginner</p>
                             </>
@@ -93,7 +93,7 @@ const EnrolledCourseCard = (props) => {
                 <div className='flex flex-col justify-center items-center min-w-[200px] gap-6 lg:max-w-xs flex-1 py-3 px-3'>
                     <div className='flex row justify-end items-center w-full px-6'>
                         <svg xmlns="http://www.w3.org/2000/svg" width="4" height="18" viewBox="0 0 4 18" fill="none">
-                            <path d="M2 2L2 2.01M2 9L2 9.01M2 16L2 16.01M2 3C1.44772 3 1 2.55228 1 2C1 1.44772 1.44772 1 2 1C2.55228 1 3 1.44772 3 2C3 2.55228 2.55228 3 2 3ZM2 10C1.44771 10 1 9.55228 1 9C1 8.44772 1.44771 8 2 8C2.55228 8 3 8.44772 3 9C3 9.55228 2.55228 10 2 10ZM2 17C1.44771 17 0.999999 16.5523 0.999999 16C0.999999 15.4477 1.44771 15 2 15C2.55228 15 3 15.4477 3 16C3 16.5523 2.55228 17 2 17Z" stroke="#111827" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M2 2L2 2.01M2 9L2 9.01M2 16L2 16.01M2 3C1.44772 3 1 2.55228 1 2C1 1.44772 1.44772 1 2 1C2.55228 1 3 1.44772 3 2C3 2.55228 2.55228 3 2 3ZM2 10C1.44771 10 1 9.55228 1 9C1 8.44772 1.44771 8 2 8C2.55228 8 3 8.44772 3 9C3 9.55228 2.55228 10 2 10ZM2 17C1.44771 17 0.999999 16.5523 0.999999 16C0.999999 15.4477 1.44771 15 2 15C2.55228 15 3 15.4477 3 16C3 16.5523 2.55228 17 2 17Z" stroke="#111827" strokeWidth="2" strokeLinecap="round" stroke-linejoin="round"/>
                         </svg>
                     </div>
                     <button className='py-2 rounded w-full bg-cyan-700 font-semibold text-white'>Continue</button>
