974015e2310b8455d74f28bdacee5faba38a423d
DandyJoshua14 <dandyjoshua14@gmail.com>
2023-10-16 18:09:42 +0100

    Finished Changes

--- src/components/cartAndPaymentComponents/CartHero.jsx
+++ src/components/cartAndPaymentComponents/CartHero.jsx
@@ -36,11 +36,12 @@ import React from 'react'
                 </div>
 
               <div className=" lg:flex justify-between "> {/* WRAPPER TO FLEX THE DESKTOP VIEW  */}
+              <div>
                 <div className=" md:flex justify-between items-center border space-x-10 p-3 rounded-2xl border-[#e7e7e7]">
                   <div className="relative">
-                    <img src={courseSelected} alt="" className='rounded-s-lg'/>
+                    <img src={courseSelected} alt="" className='rounded-lg md:rounded-s-lg lg:rounded-s-lg'/>
                     <div className="absolute top-0 bg-white left-0 m-2 text-black p-2 rounded-full text-[10px] font-bold">
-    2500 <span className="font-normal">already enrolled!</span>
+    2,500 <span className="font-normal">already enrolled!</span>
   </div>
                   </div>
 
@@ -70,14 +71,16 @@ import React from 'react'
                         <BsClockHistory />&nbsp;2hrs 30m 
                       </p>
 
-                      <p className=" ">
-                        Beginner 
+                      <p className="flex">
+                        <span><svg xmlns="http://www.w3.org/2000/svg" width="20" height="21" viewBox="0 0 20 21" fill="none">
+<path d="M7.5 16.3333V11.3333C7.5 10.4129 6.75381 9.66667 5.83333 9.66667H4.16667C3.24619 9.66667 2.5 10.4129 2.5 11.3333V16.3333C2.5 17.2538 3.24619 18 4.16667 18H5.83333C6.75381 18 7.5 17.2538 7.5 16.3333ZM7.5 16.3333V8C7.5 7.07953 8.24619 6.33333 9.16667 6.33333H10.8333C11.7538 6.33333 12.5 7.07953 12.5 8V16.3333M7.5 16.3333C7.5 17.2538 8.24619 18 9.16667 18H10.8333C11.7538 18 12.5 17.2538 12.5 16.3333M12.5 16.3333V4.66667C12.5 3.74619 13.2462 3 14.1667 3H15.8333C16.7538 3 17.5 3.74619 17.5 4.66667V16.3333C17.5 17.2538 16.7538 18 15.8333 18H14.1667C13.2462 18 12.5 17.2538 12.5 16.3333Z" stroke="black" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
+</svg></span>&nbsp;Beginner 
                       </p>
                     </div>
 
                     <div className=" inline-flex">
                       <button className=' border px-4 py-2 font-bold rounded-md border-black hover:bg-gray-400 hover:font-extrabold'>
-                        15,000
+                        15,000 NGN
                       </button>
 
                       <p className=" line-through px-4 py-2 font-bold ml-9">
@@ -85,6 +88,16 @@ import React from 'react'
                       </p>
                     </div>
                   </div>
+                </div>
+                <div className=" space-x-10 mt-5 flex justify-end">
+            <button className="border rounded-md font-semibold text-[15px] px-3 py-2 hover:bg-red-500 hover:border-red-500">
+              Remove
+            </button>
+
+            <button className="border rounded-md font-semibold text-[15px] px-3 py-2 hover:bg-yellow-500 hover:border-yellow-500">
+              Save for later
+            </button>
+          </div>
                 </div>
 
 
@@ -117,15 +130,7 @@ import React from 'react'
             </div>
           </div>
 
-          <div className=" space-x-10 mt-5">
-            <button className="">
-              Remove
-            </button>
-
-            <button className="">
-              Save for later
-            </button>
-          </div>
+         
         </div>
       </section>
     </>
