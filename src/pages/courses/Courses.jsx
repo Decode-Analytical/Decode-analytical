2d98444f4a40b381781cc2f10c63ae7c96fc1c9c
DandyJoshua14 <dandyjoshua14@gmail.com>
2023-10-18 23:59:57 +0100

    Transformed Course Hero into Component

--- src/pages/courses/Courses.jsx
+++ src/pages/courses/Courses.jsx
@@ -1,31 +1,11 @@
-import React from "react";
+import React from 'react';
+import CourseHero from '../../components/CourseHero/Coursehero';
 
 
 export default function Courses() {
-    return(
-        <>
-        
-
-        <section className="cart-bg p-[3rem] lg:p-[8rem] text-white bg-cover w-100% font-montserrat whitespace-break-spaces">
-            <h1 className="font-extrabold text-6xl pb-11">Courses</h1>
-            <p className="text-2xl">Empower Your Digital Journey With Us</p>
-            <div className="flex text-center left-[20px] text-black mt-4 max-w-[50rem] rounded-md">
-            <div className="dropdown relative inline-block">
-  <button className="addbtn bg-white p-[20px] rounded-s-md ">All</button>
-  <div className="dropdown-content hidden absolute bg-[#f1f1f1] min-w-[160px] shadow-md z-10">
-    <a className="text-black px-[12px] py-[16px] no-underline block hover:bg-[#ddd]"href="#">Course 1</a>
-    <a className="text-black px-[12px] py-[16px] no-underline block hover:bg-[#ddd]"href="#">Course 2</a>
-    <a className="text-black px-[12px] py-[16px] no-underline block hover:bg-[#ddd]"href="#">Course 3</a>
-  </div>
-</div>
-                <input placeholder="Search All Courses" className="bg-white p-[20px] w-full outline-none rounded-e-md border-none"/>
-            </div>
-            <div className="flex justify-start gap-3 p-[2rem] text-black">
-                <p className="p-[10px] px-[20px] rounded-full bg-white">Free</p>
-                <p className="p-[10px] px-[20px] rounded-full bg-white">Paid</p>
-                <p className="p-[10px] rounded-full bg-white">All Courses</p>
-            </div>
-        </section>
-        </>
+    return (
+      <>
+          <CourseHero />
+      </>
     )
-}
+  }
