6799b5ae3ec773b516ad5cfc3966af060ff769e0
DandyJoshua14 <dandyjoshua14@gmail.com>
2023-10-19 13:50:06 +0100

    Added Course Cards

--- src/components/CourseHero/Coursehero.jsx
+++ src/components/CourseHero/Coursehero.jsx
@@ -1,31 +1,63 @@
 import React from "react";
 
-
 export default function CourseHero() {
-    return(
-        <>
-        
-
-        <section className="cart-bg p-[3rem] lg:p-[8rem] text-white bg-cover w-100% font-montserrat whitespace-break-spaces">
-            <h1 className="font-extrabold text-6xl pb-11">Courses</h1>
-            <p className="text-2xl">Empower Your Digital Journey With Us</p>
-            <div className="flex text-center left-[20px] text-black mt-4 max-w-[50rem] rounded-md">
-            <div className="dropdown relative inline-block">
-  <button className="addbtn bg-white p-[20px] rounded-s-md ">All</button>
-  <div className="dropdown-content hidden absolute bg-[#f1f1f1] min-w-[160px] shadow-md z-10">
-    <a className="text-black px-[12px] py-[16px] no-underline block hover:bg-[#ddd]"href="#">Course 1</a>
-    <a className="text-black px-[12px] py-[16px] no-underline block hover:bg-[#ddd]"href="#">Course 2</a>
-    <a className="text-black px-[12px] py-[16px] no-underline block hover:bg-[#ddd]"href="#">Course 3</a>
-  </div>
-</div>
-                <input placeholder="Search All Courses" className="bg-white p-[20px] w-full outline-none rounded-e-md border-none"/>
+  let searchItem = "Graphics Design";
+  return (
+    <>
+      <section className="z-10 cart-bg p-[3rem] lg:p-[8rem] text-white bg-cover w-100% font-montserrat whitespace-break-spaces">
+        <h1 className="font-extrabold text-6xl pb-11">Courses</h1>
+        <p className="text-2xl">Empower Your Digital Journey With Us</p>
+        <div className="flex text-center left-[20px] text-black mt-4 max-w-[50rem] rounded-md">
+          <div className="dropdown relative inline-block">
+            <button className="addbtn bg-white p-[20px] rounded-s-md ">
+              All
+            </button>
+            <div className="dropdown-content hidden absolute bg-[#f1f1f1] min-w-[160px] shadow-md z-10">
+              <a
+                className="text-black px-[12px] py-[16px] no-underline block hover:bg-[#ddd]"
+                href="#"
+              >
+                Category 1
+              </a>
+              <a
+                className="text-black px-[12px] py-[16px] no-underline block hover:bg-[#ddd]"
+                href="#"
+              >
+                Category 2
+              </a>
+              <a
+                className="text-black px-[12px] py-[16px] no-underline block hover:bg-[#ddd]"
+                href="#"
+              >
+                Category 3
+              </a>
             </div>
-            <div className="flex justify-start gap-3 p-[2rem] text-black">
-                <p className="p-[10px] px-[20px] rounded-full bg-white">Free</p>
-                <p className="p-[10px] px-[20px] rounded-full bg-white">Paid</p>
-                <p className="p-[10px] rounded-full bg-white">All Courses</p>
-            </div>
-        </section>
-        </>
-    )
+          </div>
+          <input
+            placeholder="Search All Courses"
+            className="bg-white p-[20px] w-full outline-none rounded-e-md border-none"
+          />
+        </div>
+        <div className="flex justify-start gap-3 py-[2rem] text-black">
+          <p className="p-[10px] px-[20px] rounded-full bg-white">Free</p>
+          <p className="p-[10px] px-[20px] rounded-full bg-white">Paid</p>
+          <p className="p-[10px] rounded-full bg-white">All Courses</p>
+        </div>
+      </section>
+      <section className="course-items font-montserrat">
+        <h5 className="p-[2rem] text-right">
+          Showing results for: <span className="font-bold">{searchItem}</span>
+        </h5>
+        <div className="mx-auto px-[2rem]">
+          <h2 className="font-extrabold text-5xl p-[2rem]">
+            Find Trending Courses
+          </h2>
+          <p className="font-normal px-[2rem]">
+            Break into tech with any of the courses we have specially created
+            for you
+          </p>
+        </div>
+      </section>
+    </>
+  );
 }
--- src/pages/courses/Courses.jsx
+++ src/pages/courses/Courses.jsx
@@ -1,11 +1,73 @@
 import React from 'react';
 import CourseHero from '../../components/CourseHero/Coursehero';
-
+import CoursesCard from '../../components/CourseHero/CoursesCard';
+import pattern from '../../assets/courses Images/Courses.jpeg'
 
 export default function Courses() {
+  const courseDetails = [
+    {
+      image: pattern,
+      title: "Graphics Design",
+  level: "Beginner",
+  time: "2hrs 30m",
+  priceBefore: "45000",
+  priceNow: "15000"
+    },
+    {
+      image: pattern,
+      title: "Graphics Design",
+  level: "Beginner",
+  time: "2hrs 30m",
+  priceBefore: "45000",
+  priceNow: "15000"
+    }, {
+      image: pattern,
+      title: "Graphics Design",
+  level: "Beginner",
+  time: "2hrs 30m",
+  priceBefore: "45000",
+  priceNow: "15000"
+    },
+    {
+      image: pattern,
+      title: "Graphics Design",
+  level: "Beginner",
+  time: "2hrs 30m",
+  priceBefore: "45000",
+  priceNow: "15000"
+    },
+    {
+      image: pattern,
+      title: "Graphics Design",
+  level: "Beginner",
+  time: "2hrs 30m",
+  priceBefore: "45000",
+  priceNow: "15000"
+    },
+    {
+      image: pattern,
+      title: "Graphics Design",
+  level: "Beginner",
+  time: "2hrs 30m",
+  priceBefore: "45000",
+  priceNow: "15000"
+    }
+  ]
     return (
       <>
+      <div className='mx-auto font-montserrat z-10'>
           <CourseHero />
+          <section className="mx-auto items-center grid grid-cols-1 pt-20 pb-14 overflow-hidden md:grid-cols-2 lg:grid-cols-3 gap-10 place-content-center ml-11">
+          {courseDetails.map((details, index) => {
+            return <CoursesCard key={index + 1} {...details} />;
+          })}
+          <br />
+          <br />
+        </section>
+        <div className='text-center items-center p-3'>
+        <button className='p-[1rem] rounded-md border-[2px] font-bold'>View More Courses</button>
+        </div>
+        </div>
       </>
     )
   }
