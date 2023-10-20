01dd3e03c8f09e4096d6eea8b76b8bda943eed89
mawzis <olorunsholamawzis@gmail.com>
2023-10-20 19:25:09 +0100

    premium and free courses updated to ba stable

--- src/components/coursesComponents/free/Curriculum.jsx
+++ src/components/coursesComponents/free/Curriculum.jsx
@@ -6,26 +6,26 @@ export default function Curriculum() {
         <section className='max-w-[1000px] m-auto'>
             <div className=" px-[5%] md:w-[90%]">
                 <div className="">
-                    <p className=" text-3xl mt-5">
+                    <p className=" text-3xl font-bold mt-20">
                         Curriculum 
                     </p>
 
-                    <p className=" text-xs md:text-sm">
+                    <p className=" text-xs md:text-sm mt-10">
                         This course will teach you the fundamentals of UI design and how to create visually appealing user interfaces. You will study the fundamental tools, layouts, mockups, and techniques that product designers use to create remarkable interfaces.
                     </p>
                 </div>
 
-                <div className=" mt-5 flex justify-between">
-                    <p className=" font-bold">
+                <div className=" mt-14 flex justify-between">
+                    <p className=" text-2xl font-bold">
                         14 Courses
                     </p>
 
-                    <p className=" hidden md:block font-bold">
+                    <p className="text-2xl hidden md:block font-bold">
                         What you will cover in this course
                     </p>
                 </div>
 
-                <div className="">
+                <div className=" mt-10">
                     <div className=" mt-1 md:flex justify-between">
                         <p className="">
                             Introduction to UI/UX design
@@ -38,7 +38,7 @@ export default function Curriculum() {
 
                     {/* THIS DOWN WILL BE DELETED WHEN WE HAVE ACCESS TO THE BACKEND WE WILL MAP THROUGH */}
 
-                    <div className=" mt-5 md:flex justify-between">
+                    <div className=" mt-10 md:flex justify-between">
                         <p className="">
                             Introduction to UI/UX design
                         </p>
@@ -48,7 +48,7 @@ export default function Curriculum() {
                         </p>
                     </div>
 
-                    <div className=" mt-5 md:flex justify-between">
+                    <div className=" mt-10 md:flex justify-between">
                         <p className="">
                             Introduction to UI/UX design
                         </p>
@@ -58,7 +58,7 @@ export default function Curriculum() {
                         </p>
                     </div>
 
-                    <div className=" mt-5 md:flex justify-between">
+                    <div className=" mt-10 md:flex justify-between">
                         <p className="">
                             Introduction to UI/UX design
                         </p>
@@ -68,7 +68,7 @@ export default function Curriculum() {
                         </p>
                     </div>
 
-                    <div className=" mt-5 md:flex justify-between">
+                    <div className=" mt-10 md:flex justify-between">
                         <p className="">
                             Introduction to UI/UX design
                         </p>
