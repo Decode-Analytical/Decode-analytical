ba692a0bc870e5db23b034ee515ab4fda5380a3c
Olayinka <olayinka4grace@gmail.com>
2023-10-24 08:48:36 +0100

    commit

--- src/components/courseUploadComponents/Form.jsx
+++ src/components/courseUploadComponents/Form.jsx
@@ -2,6 +2,11 @@ import React, { useState } from "react";
 
 export default function Form() {
   const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("");
+  const [level, setLevel] = useState("");
+  const [description, setDescription] = useState("");
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
   return (
     <div>
       <p>Course Information</p>
@@ -18,7 +23,7 @@ export default function Form() {
         />
         <div className="grid sm:grid-cols-2 gap-1 mb-3">
           <div>
-            <label for="category" className="font-bold text-sm block">
+            <label htmlFor="category" className="font-bold text-sm block">
               Category
             </label>
             <select className="w-[50%] inline-block">
@@ -28,7 +33,7 @@ export default function Form() {
             </select>
           </div>
           <div>
-            <label for="level" className="font-bold text-sm block">
+            <label htmlFor="level" className="font-bold text-sm block">
               Level
             </label>
             <select className="w-[50%]">
@@ -38,31 +43,33 @@ export default function Form() {
             </select>
           </div>
         </div>
-        <label for="level" className="font-bold text-sm">
+        <label htmlFor="level" className="font-bold text-sm">
           Description
         </label>
         <textarea
           cols={10}
           rows={5}
           placeholder="e.g introduction to data analysis"
-          onChange={(e) => setTitle(e.target.value)}
+          onChange={(e) => setDescription(e.target.value)}
           className="p-1 mb-3 border border-zinc-200 w-[100%]"
-          value={title}
+          value={description}
         />
-         <label htmlFor="title" className="font-bold text-sm">
+        <label htmlFor="question" className="font-bold text-sm">
           Frequently asked questions
         </label>
         <input
           type="text"
+          onChange={(e) => setQuestion(e.target.value)}
           placeholder="e.g how much do you charge per call"
           className="p-1 mb-3 w-[100%]"
-          value={title}
+          value={question}
         />
-         <input
+        <input
           type="text"
+          onChange={(e) => setAnswer(e.target.value)}
           placeholder="e.g a call cost...."
           className="p-1 mb-3 w-[100%]"
-          value={title}
+          value={answer}
         />
         <div className="flex flex-wrap justify-between text-xs">
           <button className="border border-zinc-400 p-2">Save as draft</button>
