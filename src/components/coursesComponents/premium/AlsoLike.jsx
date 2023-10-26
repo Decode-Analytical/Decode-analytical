85522fd1c7f42d7ddcaf2e2a3e0349343dbc431b
mawzis <olorunsholamawzis@gmail.com>
2023-10-26 11:27:36 +0100

    view all courses pages, premium and free pages updated with API

--- src/components/coursesComponents/premium/AlsoLike.jsx
+++ src/components/coursesComponents/premium/AlsoLike.jsx
@@ -1,13 +1,10 @@
-import React, {useState, useEffect} from 'react'
-import Axios from 'axios'
-
-import Ellipse from '../../../assets/courses Images/Ellipse.png'
-
-
-import {BsClockHistory} from "react-icons/bs"
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+import Ellipse from '../../../assets/courses Images/Ellipse.png';
+import { BsClockHistory } from 'react-icons/bs';
 
 export default function AlsoLike() {
-    const courseURL = "https://decode-mnjh.onrender.com/api/course/viewAllCourses";
+    const courseURL = 'https://decode-mnjh.onrender.com/api/course/viewAllCourses';
     const apiKey = import.meta.env.VITE_API_KEY;
 
     const token = apiKey;
@@ -15,104 +12,95 @@ export default function AlsoLike() {
     // VIEW THE COURSES STATE
     const [courses, setCourses] = useState([]);
 
-
-
     // VIEW DETAILS BUTTON HANDLER, SO ONE BUTTON DOES NOT TRIGGER ALL BUTTONS TO ACTION
     const [courseDetails, setCourseDetails] = useState({});
 
     const toggleDetails = (courseId) => {
         setCourseDetails((prevState) => ({
-          ...prevState,
-          [courseId]: !prevState[courseId],
+            ...prevState,
+            [courseId]: !prevState[courseId],
         }));
-      };
+    };
 
-        useEffect(() => {
-            const fetchCourses = async () => {
+    //   FETCHING THE API
+    useEffect(() => {
+        const fetchCourses = async () => {
             try {
                 const response = await Axios.get(courseURL, {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
                 });
-        
+
                 if (response.data && response.data.courses) {
-                setCourses(response.data.courses);
-                console.log(response.data)
+                    setCourses(response.data.courses);
+                    console.log(response.data);
                 }
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
-            };
-        
-            fetchCourses();
-        }, [token]);
-    
-  return (
-    <>
-        <section className=" mx-[5%] mt-20 mb-20">
-            
-            <div className="max-w-[1100px] m-auto">
-                <h2 className=" text-3xl">
-                    You might also like
-                </h2>
-            </div>
-        
-            
-            <div className="max-w-[1100px] m-auto md:flex justify-between">
-                {courses.map((course, index) => (
-                    <div className=' w-full md:max-w-[350px] px-2' key={course._id} >
-                        <div className=" mt-5 border p-3 rounded-lg border-gray">
-                            <div className=" ">
-                                <img src={course.course_image[0].path} alt="" className=' w-full'/>
+        };
 
-                                <p className=" font-bold mt-2 mx-3">
-                                    {course.course_title}
-                                </p>
+        fetchCourses();
+    }, [token]);
 
-                                <div className=" mt-2 flex items-center mx-3">
-                                    <img src={Ellipse} alt="" />
-                                    <p className=" ml-3">
-                                        by Jane Doe
-                                    </p>
-                                </div>
+    // Divide the courses into groups of 3, that is makin the CSS display maximum of 3 per row
+    const coursesInGroups = [];
+    for (let i = 0; i < courses.length; i += 3) {
+        coursesInGroups.push(courses.slice(i, i + 3));
+    }
 
-                                <p className="mx-3">
-                                    
-                                    Rating: "ratings"
-                                </p>
+    return (
+        <>
+            <section className="mx-[5%] mt-20 mb-20">
+                <div className="max-w-[1100px] m-auto">
+                    <h2 className="text-3xl">You might also like</h2>
+                </div>
 
-                                <div className=" mt-3 flex justify-between mx-3">
-                                    <p className=" flex items-center space-x-2">
-                                    <BsClockHistory /> <span>2h 30m</span>
-                                    </p>
+                <div className="max-w-[1100px] m-auto">
+                    {coursesInGroups.map((courseGroup, groupIndex) => (
+                        <div className="md:flex justify-between" key={groupIndex}>
+                            {courseGroup.map((course) => (
+                                <div className="w-full md:max-w-[350px] px-2" key={course._id}>
+                                    <div className="mt-5 border p-3 rounded-lg border-gray">
+                                        {course.course_image && course.course_image[0] && course.course_image[0].path && (
+                                            <img src={course.course_image[0].path} alt="" className="w-full" />
+                                        )}
 
-                                    <p className="">
-                                        Beginners
-                                    </p>
-                                </div>
+                                        <p className="font-bold mt-2 mx-3">{course.course_title}</p>
 
-                                <div className=" mt-2 flex justify-between mx-3">
-                                    <button onClick={() => toggleDetails(course._id)}>
-                                        View Course
-                                    </button>
+                                        <div className="mt-2 flex items-center mx-3">
+                                            <img src={Ellipse} alt="" />
+                                            <p className="ml-3">by Jane Doe</p>
+                                        </div>
 
-                                    <p className="">
-                                        {course.isPaid_course}
-                                    </p>
-                                </div>
+                                        <p className="mx-3">Rating: {course.ratings}</p>
 
-                                {courseDetails[course._id] && (
-                                    <div className="">
-                                        {course.course_description}
+                                        <div className="mt-3 flex justify-between mx-3">
+                                            <p className="flex items-center space-x-2">
+                                                <BsClockHistory />
+                                                <span>2h 30m</span>
+                                            </p>
+
+                                            <p className="">Beginners</p>
+                                        </div>
+
+                                        <div className="mt-2 flex justify-between mx-3">
+                                            <button onClick={() => toggleDetails(course._id)}>View Course</button>
+
+                                            <p className="">{course.isPaid_course}</p>
+                                        </div>
+
+                                        {courseDetails[course._id] && (
+                                            <div className="text-gray text-xs w-[90%] m-auto">{course.course_description}</div>
+                                        )}
                                     </div>
-                                )}
-                            </div>
+                                </div>
+                            ))}
                         </div>
-                    </div>
-                ))}
-            </div>
-        </section>
-    </>
-  )
+                    ))}
+                </div>
+            </section>
+        </>
+    );
 }
--- src/components/coursesComponents/premium/CourseOverview.jsx
+++ src/components/coursesComponents/premium/CourseOverview.jsx
@@ -1,91 +1,89 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
 
 export default function CourseOverview() {
-  return (
-    <>
-        <section className='max-w-[1000px] m-auto'>
-            <div className=" px-[5%] md:w-[90%]">
-                <div className="">
-                    <p className=" text-3xl mt-20 font-bold">
-                        Course Overview 
-                    </p>
+    const courseURL = 'https://decode-mnjh.onrender.com/api/course/viewAllCourses';
+    const apiKey = import.meta.env.VITE_API_KEY;
+    const token = apiKey;
 
-                    <p className=" text-xs md:text-sm mt-5">
-                        This course will teach you the fundamentals of UI design and how to create visually appealing user interfaces. You will study the fundamental tools, layouts, mockups, and techniques that product designers use to create remarkable interfaces.
-                    </p>
-                </div>
+    // VIEW THE COURSES STATE
+    const [courses, setCourses] = useState([]);
+    const [showAllCourses, setShowAllCourses] = useState(false);
 
-                <div className=" mt-10 flex justify-between">
-                    <p className=" font-bold text-2xl">
-                        14 Courses
-                    </p>
+    useEffect(() => {
+        const fetchCourses = async () => {
+            try {
+                const response = await Axios.get(courseURL, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
 
-                    <p className=" hidden md:block font-bold text-2xl">
-                        What you will cover in this course
-                    </p>
-                </div>
+                if (response.data && response.data.courses) {
+                    setCourses(response.data.courses);
+                    console.log(response.data);
+                }
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
 
-                <div className=" mt-10">
-                    <div className=" mt-1 md:flex justify-between">
-                        <p className="">
-                            Introduction to UI/UX design
-                        </p>
+        fetchCourses();
+    }, [token]);
 
-                        <p className=" text-xs">
-                            32:34 mins
-                        </p>
-                    </div>
+    const visibleCourses = showAllCourses ? courses : courses.slice(0, 3);
+    const buttonText = showAllCourses ? 'View Less' : 'View All';
 
-                    {/* THIS DOWN WILL BE DELETED WHEN WE HAVE ACCESS TO THE BACKEND WE WILL MAP THROUGH */}
-
-                    <div className=" mt-10 md:flex justify-between">
-                        <p className="">
-                            Introduction to UI/UX design
-                        </p>
-
-                        <p className=" text-xs">
-                            32:34 mins
-                        </p>
-                    </div>
-
-                    <div className=" mt-10 md:flex justify-between">
-                        <p className="">
-                            Introduction to UI/UX design
-                        </p>
-
-                        <p className=" text-xs">
-                            32:34 mins
-                        </p>
-                    </div>
-
-                    <div className=" mt-10 md:flex justify-between">
-                        <p className="">
-                            Introduction to UI/UX design
-                        </p>
-
-                        <p className=" text-xs">
-                            32:34 mins
-                        </p>
-                    </div>
-
-                    <div className=" mt-10 md:flex justify-between">
-                        <p className="">
-                            Introduction to UI/UX design
-                        </p>
-
-                        <p className=" text-xs">
-                            32:34 mins
-                        </p>
-                    </div>
-
+    return (
+        <>
+            <section className='max-w-[1000px] m-auto'>
+                <div className="px-[5%] md:w-[90%]">
                     <div className="">
-                        <button className=' mt-5 w-28 border border-purple-700 text-purple-700 text-lg rounded-md hover:bg-purple-300'>
-                            View All
-                        </button>
+                        <p className="text-3xl mt-20 font-bold">
+                            Course Overview 
+                        </p>
+
+                        <p className="text-xs md:text-sm mt-5">
+                            This course will teach you the fundamentals of UI design and how to create visually appealing user interfaces. You will study the fundamental tools, layouts, mockups, and techniques that product designers use to create remarkable interfaces.
+                        </p>
+                    </div>
+
+                    <div className="mt-10 flex justify-between">
+                        <p className="font-bold text-2xl">
+                            {showAllCourses ? `${courses.length} Courses` : '3 Courses'}
+                        </p>
+
+                        <p className="hidden md:block font-bold text-2xl">
+                            What you will cover in this course
+                        </p>
+                    </div>
+
+                    <div className="mt-10 space-y-5">
+                        {visibleCourses.map((course, index) => (
+                            <div className="mt-1 md:flex justify-between" key={course._id}>
+                                <p className="">
+                                    {course.course_title}
+                                </p>
+
+                                <p className="text-xs">
+                                    32:34 mins
+                                </p>
+                            </div>
+                        ))}
+
+                        {courses.length > 3 && (
+                            <div className="">
+                                <button
+                                    className='mt-5 w-28 border border-purple-700 text-purple-700 text-lg rounded-md hover:bg-purple-300'
+                                    onClick={() => setShowAllCourses(!showAllCourses)}
+                                >
+                                    {buttonText}
+                                </button>
+                            </div>
+                        )}
                     </div>
                 </div>
-            </div>
-        </section>
-    </>
-  )
+            </section>
+        </>
+    );
 }
