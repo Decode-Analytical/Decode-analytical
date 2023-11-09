1a4fca192d4a123fb733028054c40fbe28686431
Hassaan Baig <hassaanb000@gmail.com>
2023-11-09 14:00:50 +0500

    Add admin Login

--- src/pages/AllPages.jsx
+++ src/pages/AllPages.jsx
@@ -1,101 +1,46 @@
-import React, {useEffect,useState} from 'react'
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import PaystackCard from '../components/cartAndPaymentComponents/PaystackCard';
-
+import PaystackCard from "../components/cartAndPaymentComponents/PaystackCard";
 
 export default function AllPages() {
-    const[admin, setAdmin] =useState(false)
-    useEffect(() => {
-        function checkUser() {
-            let userLoginData = JSON.parse(localStorage.getItem("user"))
-            if (userLoginData?.user.role == "admin") {
-                setAdmin(true)
-            } else {
-                setAdmin(false)
-            }
-        }
-        checkUser()
-      }, []);
+  const [admin, setAdmin] = useState(false);
+  useEffect(() => {
+    function checkUser() {
+      let userLoginData = JSON.parse(localStorage.getItem("user"));
+      if (userLoginData?.user.role == "admin") {
+        setAdmin(true);
+      } else {
+        setAdmin(false);
+      }
+    }
+    checkUser();
+  }, []);
   return (
-    <div className=' text-center'>
-        <Link to="/">
-            HOME
-        </Link> <br />
-
-        <Link to="/login">
-            LoginSignUp
-        </Link> <br />
-        
-
-        <Link to="/resetpassword">
-            PasswordReset
-        </Link> <br />
-
-        <Link to="/FreeCourses">
-            FreeCourses
-        </Link> <br />
-
-
-        <Link to="/CartPage">
-            CartPage
-        </Link><br />
-
-        <Link to="/PaymentPage">
-            PaymentPage
-        </Link> <br />
-
-        <Link to="/Dashboard">
-            Dashboard
-        </Link> <br />
-
-        <Link to="/NotEnrolled">
-            NotEnrolled
-        </Link> <br />
-
-        <Link to="/ForumIntro">
-            ForumIntro
-        </Link> <br />
-
-        <Link to="/InForumReply">
-         InForumReply
-        </Link> <br />
-
-        <Link to="/WeeklyForumPage">
-            WeeklyForumPage
-        </Link> <br />
-
-        {/* <Link to="/dashboard">
+    <div className=" text-center">
+      <Link to="/">HOME</Link> <br />
+      <Link to="/login">LoginSignUp</Link> <br />
+      <Link to="/resetpassword">PasswordReset</Link> <br />
+      <Link to="/FreeCourses">FreeCourses</Link> <br />
+      <Link to="/CartPage">CartPage</Link>
+      <br />
+      <Link to="/PaymentPage">PaymentPage</Link> <br />
+      <Link to="/Dashboard">Dashboard</Link> <br />
+      <Link to="/NotEnrolled">NotEnrolled</Link> <br />
+      <Link to="/ForumIntro">ForumIntro</Link> <br />
+      <Link to="/InForumReply">InForumReply</Link> <br />
+      <Link to="/WeeklyForumPage">WeeklyForumPage</Link> <br />
+      {/* <Link to="/dashboard">
             dashboard
         </Link> <br /> */}
-
-        <Link to="/mycourses">
-            mycourses
-        </Link> <br />
-
-        <Link to="/Courses">
-            Courses
-        </Link> <br />
-
-        <Link to="/profile/*">
-            profile/*
-        </Link> <br />
-
-        <Link to="/PurchasedPage">
-            PurchasedPage
-        </Link> <br />
-
-        <Link to="/InstrucructorProfile">
-            Instructor profile
-        </Link><br />
-
-         <Link to="/courseinfo">
-            course Upload
-        </Link>
-        {admin && (
-            <Link to="/AdminDashboard">
-                Admin Dashboard
-            </Link>
-        )}
+      <Link to="/mycourses">mycourses</Link> <br />
+      <Link to="/Courses">Courses</Link> <br />
+      <Link to="/profile/*">profile/*</Link> <br />
+      <Link to="/PurchasedPage">PurchasedPage</Link> <br />
+      <Link to="/InstrucructorProfile">Instructor profile</Link>
+      <br />
+      <Link to="/courseinfo">course Upload</Link> <br />
+      <Link to="/AdminLogin">Admin Login</Link> <br />
+      <Link to="/AdminDashboard">Admin Dashboard</Link> <br />
     </div>
-  )
+  );
 }
