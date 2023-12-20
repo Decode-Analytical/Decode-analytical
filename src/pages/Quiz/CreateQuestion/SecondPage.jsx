22254081b63cdc24d104aa311421b02caf8a1fd8
Owner <owner@ROY-Macbook-pro.local>
2023-12-20 19:14:34 +0200

    Quiz page  added

--- src/pages/Quiz/CreateQuestion/SecondPage.jsx
+++ src/pages/Quiz/CreateQuestion/SecondPage.jsx
@@ -12,7 +12,7 @@ const SecondPage = () => {
     <div className='w-auto h-auto'>
     <div className='w-full flex items-center pl-4 pr-4 justify-between mt-2'>
           <div className='flex text-purple-950'>
-                <div>
+                <div onClick={()=>handleClick()}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" dataSlot="icon" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
                     </svg>
