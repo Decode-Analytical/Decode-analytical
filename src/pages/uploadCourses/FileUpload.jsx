bbeade15f796fdd1208d92bc8f65e75884cc76db
Hassaanjbaig-code <hassaanb000@gmail.com>
2023-12-20 16:43:54 +0500

    Fix an error for not working in the chrome and edge

--- src/pages/uploadCourses/FileUpload.jsx
+++ src/pages/uploadCourses/FileUpload.jsx
@@ -10,7 +10,7 @@ export default function FileUpload({ onDrop, className }) {
 
   return (
     <div {...getRootProps({ className: className })}>
-      <input {...getInputProps()} required />
+      <input {...getInputProps()} />
       <div className="flex flex-col gap-2">
         <button
           type="button"
